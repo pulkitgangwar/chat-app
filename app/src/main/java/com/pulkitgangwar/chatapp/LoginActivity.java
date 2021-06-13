0986e273145e80e20df9d54a4f62141044c1ba62
pulkitgangwar <pulkitgangwar2016@gmail.com>
2021-06-13 06:08:42 +0530

    initial commit for login activity

---
+++ app/src/main/java/com/pulkitgangwar/chatapp/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.pulkitgangwar.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
